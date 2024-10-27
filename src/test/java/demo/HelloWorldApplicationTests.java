e9705015f1526045484d6e53cac08a38e9f3e8b1
Thorbjorn M <thomelheim@gmail.com>
2024-10-27 21:46:27 +0100

    github actions

--- src/test/java/demo/HelloWorldApplicationTests.java
+++ src/test/java/demo/HelloWorldApplicationTests.java
@@ -3,11 +3,18 @@ package demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class HelloWorldApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
+	@Test
+	void testAddition(){
+		int two = 2;
+		assertEquals(two, 2);
+	}
 }
